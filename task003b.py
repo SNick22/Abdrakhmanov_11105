457751de6989692e3e5b44482b1d07d0feb6c645
Azat <azat3310977@gmail.com>
2022-03-14 16:05:57 +0300

    Added 003b

--- task003b.py
+++ task003b.py
@@ -1,16 +1,60 @@
-from collections import deque
+class Stack:
+    def __init__(self):
+        self.arr = []
 
-a = input().split()
-elems = deque()
-oper = deque()
+    def push(self, value):
+        self.arr.append(value)
 
-for item in a:
-    if item not in ['-', '+', '*', '/', '(', ')']:
-        elems.append(item)
-    elif item in ['(', ')']:
-        continue
-    else:
-        oper.appendleft(item)
-result = str(elems + oper)
+    def pop(self):
+        res = self.arr.pop()
+        return res
 
-print(result)
+    def peek(self):
+        return self.arr[-1]
+
+    def isEmpty(self):
+        if len(self.arr) == 0:
+            return True
+        return False
+
+
+class Queue(Stack):
+    def push(self, value):
+        self.arr.insert(0, value)
+
+
+string = input().split()
+myStack = Stack()
+myQueue = Queue()
+for i in string:
+    if i != '+' and i != '-' and i != '*' and i != '/' and i != '(' and i != ')':
+        myQueue.push(i)
+    if i == '+' or i == '-' or i == '*' or i == '/':
+        if myStack.isEmpty() or myStack.peek() == '(':
+            myStack.push(i)
+            continue
+        if (i == '*' or i == '/') and (myStack.peek() == '-' or myStack.peek() == '+'):
+            myStack.push(i)
+        elif i == '*' or i == '/':
+            while myStack.peek() != '+' or myStack.peek() != '-' or myStack.peek() != '(':
+                myQueue.push(myStack.pop())
+                if myStack.isEmpty():
+                    break
+        else:
+            while myStack.peek() != '(':
+                myQueue.push(myStack.pop())
+                if myStack.isEmpty():
+                    break
+            myStack.push(i)
+    if i == '(':
+        myStack.push(i)
+    if i == ')':
+        while myStack.peek() != '(':
+            myQueue.push(myStack.pop())
+        myStack.pop()
+while not myStack.isEmpty():
+    myQueue.push(myStack.pop())
+output = ''
+while not myQueue.isEmpty():
+    output += myQueue.pop()
+print(output)
