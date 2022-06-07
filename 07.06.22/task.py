a52a2414625e6a9a79aab943ecebe4bc8bf87642
Azat <azat3310977@gmail.com>
2022-06-07 21:56:18 +0300

    added task

---
+++ 07.06.22/task.py
@@ -0,0 +1,27 @@
+import re
+from functools import reduce
+from collections import Counter
+
+with open('text.txt', encoding='utf-8') as file:
+    text = file.read()
+words = re.findall(r'(?:\w+‑\w+)|(?:\w+)', text)
+last_words = []
+for word in words:
+    if word.lower() not in last_words:
+        last_words.append(word.lower())
+first_letter_words = {}
+for word in words:
+    try:
+        first_letter_words[word[0].lower()] += 1
+    except KeyError:
+        first_letter_words[word[0].lower()] = 1
+sorted_words = sorted(first_letter_words.items(), key=lambda x: x[0])
+cnt = Counter(map(lambda x: x.lower(), words))
+
+with open('text.txt', 'a', encoding='utf-8') as file:
+    file.write(f'\n\n{len(last_words)}\n')
+    for i in sorted_words:
+        file.write(f'\n{i[0]}: {i[1]}')
+    file.write(f'\n\n{reduce(lambda a, b: a if len(a) > len(b) else b, words)}')
+    file.write(f'\n\n{cnt.most_common()[0][0]}')
+    
